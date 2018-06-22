f9488d5e20455401bcc88e6b28bfcf93f80850a4
morganrae <morgane@mit.edu>
2018-06-21 21:48:06 -0400

    Added city latitude and longitude to cleaning process

--- clean.py
+++ clean.py
@@ -1,18 +1,32 @@
 import csv
 import pandas as pd
-import math
+import pickle
 
 cities_data = pd.read_csv('cities_data_raw.csv',encoding='utf-8')
+geocodes = pickle.load(open('geocodes.p','rb'))
 
 # Remove extra rows:
 cities_data = cities_data[cities_data.City.str.contains("Mean") == False]
 cities_data = cities_data[cities_data.City.str.contains("Median") == False]
 cities_data = cities_data[cities_data.City.notnull() == True]
 
-# Create row with full location:
+# Create row with full city location:
 cities_data['Location'] = 0
+cities_data['Latitude'] = 0
+cities_data['Longitude'] = 0
+
 for index, row in cities_data.iterrows():
-  cities_data.loc[index,'Location'] = (cities_data.loc[index,'City'] + ", " +
+  location = (cities_data.loc[index,'City'] + ", " + 
     cities_data.loc[index,'State'] + ", USA")
 
+  if location not in geocodes.keys(): 
+    print('Missing geocode for ', location, '.')
+  else:
+    geocode = geocodes[location]
+    latitude = geocode[0]['geometry']['location']['lat']
+    longitude = geocode[0]['geometry']['location']['lng']
+    cities_data[index,'Location'] = location
+    cities_data[index,'Latitude'] = latitude
+    cities_data[index,'Longitude'] = longitude
+  
 cities_data.to_csv('cities_data_clean.csv',encoding='utf-8')
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import csv
 import pandas as pd
 
 # Load descriptive data on cities:
-cities_data = pd.read_csv('cities_data_clean.csv',encoding='utf-8')
+cities_data = pd.read_csv('cities_data_clean.csv',encoding='utf-8',
+    lineterminator='\n')
