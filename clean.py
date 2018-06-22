b9072c1b499408e12a581f64652b24d308b9c482
morganrae <morgane@mit.edu>
2018-06-21 21:36:39 -0400

    Added full city location and feature to geocode cities

--- clean.py
+++ clean.py
@@ -4,8 +4,15 @@ import math
 
 cities_data = pd.read_csv('cities_data_raw.csv',encoding='utf-8')
 
+# Remove extra rows:
 cities_data = cities_data[cities_data.City.str.contains("Mean") == False]
 cities_data = cities_data[cities_data.City.str.contains("Median") == False]
 cities_data = cities_data[cities_data.City.notnull() == True]
 
-cities_data.to_csv('cities_data_clean.csv')
+# Create row with full location:
+cities_data['Location'] = 0
+for index, row in cities_data.iterrows():
+  cities_data.loc[index,'Location'] = (cities_data.loc[index,'City'] + ", " +
+    cities_data.loc[index,'State'] + ", USA")
+
+cities_data.to_csv('cities_data_clean.csv',encoding='utf-8')
--- locate.py
+++ locate.py
@@ -2,4 +2,53 @@ import pickle
 import pandas as pd
 import googlemaps
 
-geocodes = pickle.load(open('geocodes.p', 'rb'))
+geocodes = pickle.load(open('geocodes.p','rb'))
+cities_data = pd.read_csv('cities_data_clean.csv',encoding='utf-8',
+    lineterminator='\n')
+
+failure_count = 0 
+
+for index in range(0,77):
+    location = cities_data.loc[index,'Location']
+    print('Processing',index,':',location)
+
+    if location not in geocodes.keys(): 
+        
+        with open('api.txt','r') as api_file:
+            api_key = api_file.read()
+
+        gmaps = googlemaps.Client(key=api_key)
+
+        try: 
+            geocode = gmaps.geocode(location)
+            geocodes[location] = geocode
+            print('Success!')
+    
+        except Exception as e: 
+            print('Failed attempt',e,index)
+            failure_count += 1
+
+    elif geocodes[location] == []:
+
+        print('Empty address...replacing.')
+
+        with open('api.txt','r') as api_file:
+            api_key = api_file.read()
+
+        gmaps = googlemaps.Client(key=api_key)
+
+        try: 
+            geocode = gmaps.geocode(location)
+            geocodes[location] = geocode
+            print('Success!')
+    
+        except Exception as e: 
+            print('Failed attempt', e, index)
+            failure_count += 1
+
+    else: print('Address already in dictionary.')
+
+print(len(geocodes),'addresses in dictionary.')
+print(failure_count,'failures.')
+
+pickle.dump(geocodes,open('geocodes.p','wb'))
